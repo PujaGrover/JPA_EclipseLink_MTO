2ce388f8d2fab8d2e9a7c48d33f537fc51250936
Puja Grover <puja_grover@yahoo.com>
2020-10-18 13:52:10 -0500

    Adding Development Branch To Test new Class

---
+++ src/main/java/com/tutorialspoint/eclipselink/entity/TestClass.java
@@ -0,0 +1,10 @@
+package com.tutorialspoint.eclipselink.entity;
+
+public class TestClass {
+	
+	//Testing the new branch for git
+	public TestClass() {
+		System.out.println("Testing new branch");
+	}
+
+}
